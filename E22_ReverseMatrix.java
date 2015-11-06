87c2360cd775b4740a6dbe986fb19eee4cccbd22
Sandro Dolidze <sdolidze3@gmail.com>
2015-11-06 10:25:10 +0400

    fixed formating in 22

--- E22_ReverseMatrix.java
+++ E22_ReverseMatrix.java
@@ -12,55 +12,56 @@ public class E22_ReverseMatrix {
 			{3, 2, 1},
 			{6, 5, 4},
 			{9, 8, 7}
-};
+		};
 
-int[][] horizontal = {
-	{7, 8, 9},
-	{4, 5, 6},
-	{1, 2, 3}
-};
+		int[][] horizontal = {
+			{7, 8, 9},
+			{4, 5, 6},
+			{1, 2, 3}
+		};
 
-reverseVertically(matrix);
+		reverseVertically(matrix);
 
-System.out.println(Arrays.deepEquals(matrix, vertical));
-	
-reverseVertically(matrix);
-reverseHorizontally(matrix);
-System.out.println(Arrays.deepEquals(matrix, horizontal));
-}
+		System.out.println(Arrays.deepEquals(matrix, vertical));
+			
+		reverseVertically(matrix); // undo, matrix is same as it used to be
+		reverseHorizontally(matrix);
+
+		System.out.println(Arrays.deepEquals(matrix, horizontal));
+	}
 	
 	
 	public static void reverseVertically(int[][] matrix) {
 		for (int[] row: matrix) {
 			reverse(row);
-}
-}
+		}
+	}
 
-public static void reverseHorizontally(int[][] matrix) {
-	reverse(matrix);
-}
+	public static void reverseHorizontally(int[][] matrix) {
+		reverse(matrix);
+	}
 
-public static void reverse(int[] xs) {
-	for (int i = 0; i < xs.length / 2; i++) {
-		swap(xs, i, xs.length - 1 - i);
-}
-}
+	public static void reverse(int[] xs) {
+		for (int i = 0; i < xs.length / 2; i++) {
+			swap(xs, i, xs.length - 1 - i);
+		}
+	}
 
-public static void swap(int[] xs, int i, int j) {
-	int temp = xs[i];
-	xs[i] = xs[j];
-	xs[j] = temp;
-}
+	public static void swap(int[] xs, int i, int j) {
+		int temp = xs[i];
+		xs[i] = xs[j];
+		xs[j] = temp;
+	}
 
-public static<T> void reverse(T[] xs) {
-	for (int i = 0; i < xs.length / 2; i++) {
-		swap(xs, i, xs.length - 1 - i);
-}
-}
+	public static<T> void reverse(T[] xs) {
+		for (int i = 0; i < xs.length / 2; i++) {
+			swap(xs, i, xs.length - 1 - i);
+		}
+	}	
 
-public static<T> void swap(T[] xs, int i, int j) {
-	T temp = xs[i];
-	xs[i] = xs[j];
-	xs[j] = temp;
-}
+	public static<T> void swap(T[] xs, int i, int j) {
+		T temp = xs[i];
+		xs[i] = xs[j];
+		xs[j] = temp;
+	}
 }
